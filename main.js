51ae3de7562c31ce75c736056d8fbaf9be51fc15
Muhammad Islakha <lakhatekno2514@gmail.com>
2023-05-11 20:39:22 +0700

    ya
    - description box
    - add title "popular film"
    - fix search box bug

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-// import axios from 'axios';
-
 document.body.onload = getResult("discover/movie?");
 
 function genreList(genre_ids, element) {
@@ -13,7 +11,7 @@ function genreList(genre_ids, element) {
       for(let i = 0; i<genre_ids.length; i++){
         for(let j = 0; j<genres.length; j++){
           if(genre_ids[i]===genres[j].id){
-            if(j+1==genres.length){
+            if(j-1 < genres.length){
               genre_names = genre_names + ' ' + genres[j].name + ',';
             }
             else {
@@ -34,59 +32,61 @@ function limiter(text, length) { //elipsis
   else return text.substring(0, length);
 }
 
-//eventlistener search
-const search = document.querySelector('#search');
-search.addEventListener('blur', ()=>{
-  const main = document.querySelector('main');
-  main.innerHTML = '';
-  let query = `search/movie?query=${search.value}`;
-  getResult(query);
-})
-
-// event listener <page>
 const prev = document.querySelector('#prev');
 const current = document.querySelector('#current');
 const next = document.querySelector('#next');
+
+const search = document.querySelector('#search');
+search.addEventListener('blur', ()=> {
+  current.innerText = '1';
+  if(search.value == '') {
+    getResult("discover/movie?");
+  } else {
+    let query = `search/movie?query=${search.value}`;
+    getResult(query);
+  }
+});
+
 prev.addEventListener('click', ()=>{
   if(current.innerText>'1') {current.innerText--};
+  
   if(search.value == '') {
     query = 'discover/movie?';
-  }
-  else {
+  } else {
     query = `search/movie?query=${search.value}`;
   }
-  // else alert("Already on first page");
-  const main = document.querySelector("#main");
-  main.innerHTML = '';
+
   getResult(query);
-})
+});
+
 next.addEventListener('click', ()=>{
   current.innerText++;
+
   if(search.value == '') {
     query = 'discover/movie?';
-  }
-  else {
+  } else {
     query = `search/movie?query=${search.value}`;
   }
-  const main = document.querySelector("#main");
-  main.innerHTML = '';
-  getResult(query);
-})
 
-//function untuk menampilkan cards
+  getResult(query);
+});
+
 function getResult(query) {
-const page = document.querySelector('#current').innerText;
-  axios
-.get(
+  const main = document.querySelector(".content");
+  main.innerHTML = '<h1></h1>';
+  const page = document.querySelector('#current').innerText;
+  axios.get(
   `https://api.themoviedb.org/3/${query}&api_key=ad447a5b83d006da4d6aad0b8d3b2f0e&page=${page}`
   )
   .then(function (response) {
     for (let i = 0; i < (response.data.results).length; i++) {
-      const main = document.querySelector("#main");
+      const title = document.querySelector("h1");
+      query == "discover/movie?" ? title.innerText = "Popular Film" : title.innerText = `results for "${search.value}"`;
       const card = document.createElement("div");
       card.className = "movie";
       const mov_img = document.createElement("img");
       const mov_text = document.createElement("div");
+      mov_text.className = 'desc'
       const mov_title = document.createElement("h3");
       const mov_genre = document.createElement("p");
       const mov_year = document.createElement("p");
@@ -94,24 +94,23 @@ const page = document.querySelector('#current').innerText;
       const hoverview = document.createElement("div");
       hoverview.className = 'overview';
       const overview = document.createElement("p");
-      overview.className = 'desc'; 
 
       mov_title.innerText = `${response.data.results[i].title}`;
       mov_year.innerText = limiter(`${response.data.results[i].release_date}`, 4);
       genreList(response.data.results[i].genre_ids, mov_genre);
       mov_genre.className = 'genre-list';
       mov_img.src = `https://image.tmdb.org/t/p/w500${response.data.results[i].poster_path}`;
-      overview.innerText = limiter(`${response.data.results[i].overview}`, 65);
+      overview.innerText = response.data.results[i].overview;
 
       hoverview.appendChild(mov_title);
       hoverview.appendChild(mov_year);
       hoverview.appendChild(mov_genre);
-      hoverview.appendChild(overview);
+      mov_text.appendChild(overview);
+      hoverview.appendChild(mov_text);
       card.appendChild(mov_img);
       card.appendChild(hoverview);
       main.appendChild(card);
     }
-    console.log(response.data.results);
   })
   .catch(function (error) {
     console.log(error);
